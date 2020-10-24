847048511daec8dbc1752a0b05b26fcbddf9abe2
abdulsagheer <abdulsagheeras29@gmail.com>
2020-10-24 20:43:22 +0530

    ok

--- carproject/wsgi.py
+++ carproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carproject.carproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carproject.settings')
 
 application = get_wsgi_application()
