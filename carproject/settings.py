173562bf8752b09435b2af912acc9a4d5a53ba11
abdulsagheer <abdulsagheeras29@gmail.com>
2020-11-26 15:13:33 +0530

    ok

--- carproject/settings.py
+++ carproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'ev)1@akb_kwyjkasz8e+t!fk%^*g17#2)swymx(ckqi189=7c*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["damp-springs-56935.herokuapp.com","web-tech.biz",'www.web-tech.biz']
+ALLOWED_HOSTS = ["damp-springs-56935.herokuapp.com","techsmash.xyz",'www.techsmash.xyz']
 
 LOGIN_REDIRECT_URL='accounts:dashboard'
 
